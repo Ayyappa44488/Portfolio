6983a0506619d0fda726b8b86cdc7337c0db67cf
Karri Venkata Surya Ayyappa Reddy <134079167+Ayyappa44488@users.noreply.github.com>
2023-11-10 20:21:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,4 @@ app=Flask(__name__)
 app.secret_key = 'Ayyappa'
 @app.route('/')
 def index():
-    return render_template('portfolio.html')
-if __name__=='__main__':
-    app.run(debug=True)       
+    return render_template('portfolio.html')     
